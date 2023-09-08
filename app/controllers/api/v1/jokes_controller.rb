f590119ba504de48af5862dd6d48fc0f012b79be
Austin Woods <austinwoods99@gmail.com>
2023-09-08 15:24:08 -0500

    refactor

--- app/controllers/api/v1/jokes_controller.rb
+++ app/controllers/api/v1/jokes_controller.rb
@@ -1,4 +1,4 @@
-class API::V1::JokesController < ApplicationController
+class Api::V1::JokesController < ApplicationController
   def index
     jokes = JokeFacade.new.jokes_index(params[:limit])
     render json: JokeSerializer.new(jokes)
