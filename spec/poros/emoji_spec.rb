da0520490462387ab65fbd5733d31c5d7e19177a
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-09-08 12:39:42 -0600

    test: emoji poros

---
+++ spec/poros/emoji_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Emoji do
+  before(:each) do
+    info = {
+            code: "U+1F4A9",
+            character: "💩",
+            image: "https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F4A9.png",
+            name: "pile of poo",
+            group: "smileys_emotion",
+            subgroup: "face_costume"
+          }
+    @emoji = Emoji.new(info)
+  end
+
+  describe 'initialize' do 
+    it 'exists' do 
+      expect(@emoji).to be_an Emoji
+    end
+
+    it 'has attributes' do 
+      expect(@emoji.code).to eq("U+1F4A9")
+      expect(@emoji.character).to eq("💩")
+      expect(@emoji.image).to eq('https://api-ninjas-data.s3.us-west-2.amazonaws.com/emojis/U%2B1F4A9.png')
+      expect(@emoji.name).to eq('pile of poo')
+    end
+  end
+end
