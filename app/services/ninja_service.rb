6dcc8c973076a7f83b67879da37116e9a6e6e8d8
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-09-08 11:46:19 -0600

    add ninja service with spec.

---
+++ app/services/ninja_service.rb
@@ -0,0 +1,18 @@
+class NinjaService 
+  class << self
+    def conn
+      Faraday.new("https://api.api-ninjas.com") do |f|
+        f.headers['X-Api-Key'] = ENV["API_NINJA_KEY"]
+      end
+    end
+
+    def get_url(url)
+      response = conn.get(url)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    def get_trivia(category)
+      get_url("/v1/trivia?category=#{category}&limit=10")
+    end
+  end
+end
