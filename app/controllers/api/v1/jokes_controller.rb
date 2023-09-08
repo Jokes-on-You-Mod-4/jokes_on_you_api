527f6de4bbfa6b0bda89885602de95444ec97fe9
Austin Woods <austinwoods99@gmail.com>
2023-09-08 15:11:03 -0500

    add api v1 to jokes controller

--- app/controllers/api/v1/jokes_controller.rb
+++ app/controllers/api/v1/jokes_controller.rb
@@ -1,4 +1,4 @@
-class JokesController < ApplicationController
+class API::V1::JokesController < ApplicationController
   def index
     jokes = JokeFacade.new.jokes_index(params[:limit])
     render json: JokeSerializer.new(jokes)
